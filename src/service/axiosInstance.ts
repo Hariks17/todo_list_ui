fac0470e0920cf2578de75e696c4ec887a5d0c45
Hariks17 <hariksh.94@gmail.com>
2025-01-16 21:57:15 +0530

    Added AWS URL

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://34.207.136.206:8080",
   headers: {
     "Content-Type": "application/json",
   },
